ad7dfadc93057b42958a9d6fc960a31811fc888e
yueyiyunfeng <yueyiyunfeng@163.com>
2015-07-23 16:59:11 +0800

    my_blog

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -1,3 +1,14 @@
+#coding:utf-8
 from django.db import models
 
+class Article(models.Model):
+	title = models.CharField(max_length = 100)
+	category = models.CharField(max_length =50,blank = True)
+	date_time = models.DateTimeField(auto_now_add = True) #自动设置对象增加时间
+	content = models.TextField(blank = True, null = True)
+	
+	def __unicode__(self):
+		return self.title
+	class Meta:
+		ordering = ['-date_time']
 # Create your models here.
